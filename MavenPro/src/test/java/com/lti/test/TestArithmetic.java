1d80375d90c0ca1b1ec80cdb1c95583a9435b67b
ironman19988 <abheesht1998@gmail.com>
2020-09-11 17:56:59 +0530

    Eclipse Maven Project

---
+++ MavenPro/src/test/java/com/lti/test/TestArithmetic.java
@@ -0,0 +1,25 @@
+package com.lti.test;
+
+import static org.junit.Assert.assertEquals;
+import com.lti.code.Arithmetic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.lti.code.Arithmetic;
+
+public class TestArithmetic {
+	private Arithmetic arth;
+	
+	public void init() {
+		arth = new Arithmetic();
+		
+	}
+	
+	public void testsum() {
+		assertEquals(10, arth.sum(4,6));
+	}
+	
+	
+
+}
